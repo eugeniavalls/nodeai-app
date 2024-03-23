cc864f1ab3a6c507bd00e8bc91fc66882b7c1ab4
eugeniavalls <e.vallsalfaro@gmail.com>
2024-03-23 13:26:57 +0100

    prueba grafico

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,58 @@
 import '/src/styles/Home.css'
+import '/src/styles/Header.css'
+import '/src/styles/Portfolio-score.css'
+import '/src/script.js'
+
+
 export const Home = () => {
     return(
         <>
-            <h2>Home</h2>
+            <header className='Header'>
+                <nav className='Header-nav'>
+                    <ul className='Header-ul'>Summary</ul>
+                    <ul className='Header-ul'>Holdings</ul>
+                    <ul className='Header-ul'>Risk</ul>
+                    <ul className='Header-ul'>Sector</ul>
+                    <ul className='Header-ul Header-suggestions'>Suggestions</ul>
+                    <ul className='Header-ul'>Scenario</ul>
+                    <ul className='Header-ul'>Themes</ul>
+                    <ul className='Header-ul'>News</ul>
+                </nav>
+            </header>
+
+            <section>
+                <h2>Portfolio Score Change</h2>
+                <p>This Shows how your Portfolio Score will change if you were to execute the trades below</p>
+
+                <div className='Container'>
+                    <div className='Container-graphics'>
+                    
+                        <div className='Graphic'>
+                            <p>Current Portfolio Score</p>
+                            <div className='Graphic-container'>
+                                <div className='Graphic-circular-progress'>
+                                    <span className='Graphic-progress-value'>83.75%</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='flecha'>
+                            <span>+11,33%</span>
+                            <img src="" alt="icono-flecha" />
+                        </div>
+
+                        <div className='Graphic'>
+                            <p>Current Portfolio Score</p>
+                            <div className='Graphic-container'>
+                                <div className='Graphic-circular-progress'>
+                                    <span className='Graphic-progress-value'>95%</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
---
+++ src/script.js
@@ -0,0 +1,21 @@
+let circularProgress = document.querySelector(".Graphic-circular-progress"),
+progressValue = document.querySelector(".Graphic-progress-value");
+
+let progressStarValue = 0,
+progressEndValue1 = 83,
+progressEndValue2 = 95,
+speed = 10;
+
+
+let progress1 = setInterval(()=>{
+    progressStarValue++;
+
+    progressValue.textContent = `${progressStarValue}%`
+    circularProgress.style.background = `conic-gradient(var(--graphic-color) ${progressStarValue * 3.6}deg, var(--opacity-color) 0deg)`
+
+    if(progressStarValue == progressEndValue1) {
+        clearInterval(progress1)
+    }
+
+}, speed)
+
